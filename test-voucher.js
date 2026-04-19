f20f85df0630c3dfb17b55a3909254c8ca1032cb
Nickan13 <94055452+Nickan13@users.noreply.github.com>
2026-04-19 10:49:53 +0000

    Add test script for Spiris voucher API

---
+++ test-voucher.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const axios = require("axios");
+const tokenService = require("./src/services/tokenService");
+
+const LOCATION_ID = "FZK53zttFssaKFsCr9jl";
+const SPIRIS_API = "https://eaccounting.vismaonline.com";
+
+async function main() {
+  const token = await tokenService.getAccessTokenForLocation(LOCATION_ID);
+  console.log("Got token OK");
+
+  // Hämta ett befintligt voucher för att se strukturen
+  const res = await axios.get(`${SPIRIS_API}/v2/vouchers?$top=1`, {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+
+  console.log(JSON.stringify(res.data, null, 2));
+}
+
+main().catch(console.error);
